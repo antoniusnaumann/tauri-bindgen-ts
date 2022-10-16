51bb94845cfdc104ff872467a735c1fd1c24f96e
Antonius Naumann <git@antonius.dev>
2022-10-16 19:46:18 +0200

    Add support for serialization and deserialization for entities

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 #[doc(inline)]
 pub use tauri_bindgen_ts_macro::*;
 pub use ts_rs;
+pub use serde;
 
 pub mod prelude {
     pub use tauri_bindgen_ts_macro::*;
     pub use ts_rs;
+    pub use serde;
 }
